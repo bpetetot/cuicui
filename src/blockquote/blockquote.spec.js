16dd7b8ed126661f936e0a9ccb57885b8a80190b
Benjamin Petetot <bpetetot@gmail.com>
2017-07-23 15:11:08 +0200

    finish configure jest

--- src/blockquote/blockquote.spec.js
+++ src/blockquote/blockquote.spec.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-env jest */
-
 import snap from 'snap'
 
 import Blockquote from './blockquote'
@@ -8,5 +7,7 @@ import Blockquote from './blockquote'
 const snapshot = props => snap(Blockquote)({ ...props })
 
 describe('blockquote', () => {
+  it('should add text', snapshot({ text: 'hello world' }))
   it('should add custom className', snapshot({ className: 'custom' }))
+  it('should add custom style', snapshot({ style: { color: 'red' } }))
 })
