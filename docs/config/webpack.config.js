a7eccedad4d9a39e9b93e7bb72f14f36d131dd72
Benjamin Petetot <bpetetot@gmail.com>
2017-07-23 18:47:26 +0200

    :sparkles: create documentation app (#4)

---
+++ docs/config/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+
+const dev = process.env.NODE_ENV !== 'production'
+const BUILD_DOCS = path.join(__dirname, '..', 'build')
+const SRC_DOCS = path.join(__dirname, '..', 'src')
+const SRC_CUICUI = path.join(__dirname, '..', '..', 'src')
+
+const getLoaders = (loaders) => {
+  if (dev) {
+    loaders.push('react-hot-loader/webpack')
+  }
+  loaders.push('babel-loader')
+  return loaders
+}
+
+module.exports = {
+  devtool: dev ? 'eval' : '',
+  devServer: {
+    stats: 'errors-only',
+  },
+  entry: {
+    bundle: ['./src/index.jsx'],
+  },
+  output: {
+    path: BUILD_DOCS,
+    filename: 'bundle.js',
+    publicPath: '/build',
+  },
+  resolve: {
+    modules: [SRC_DOCS, SRC_CUICUI, 'node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: getLoaders([]),
+        include: [SRC_DOCS, SRC_CUICUI],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        include: [SRC_DOCS, SRC_CUICUI],
+      },
+      {
+        test: /\.(png|svg|gif|jpg|html)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+}
---
+++ docs/src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Blockquote from '../../src/blockquote'
+
+import './app.css'
+
+const App = () => {
+  return (
+    <div>
+      <Blockquote text="Hello man" />
+    </div>
+  )
+}
+
+export default App
