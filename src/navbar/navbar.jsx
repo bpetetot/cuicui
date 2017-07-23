b7ab9806670b88dfda27bfb41d4ed62953e7569f
Benjamin Petetot <bpetetot@gmail.com>
2017-07-23 22:21:53 +0200

    :sparkles: navbar component (#9)

---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import c from 'classnames'
+
+import './navbar.css'
+
+const Navbar = ({ logo, children, className, style }) => {
+  return (
+    <header className={c('cc-navbar', className)} style={style}>
+      <div className="cc-navbar--container">
+        <div className="cc-navbar--logo">
+          {logo}
+        </div>
+        <nav className="cc-navbar--menu">
+          {children}
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+Navbar.propTypes = {
+  logo: PropTypes.node,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.array]),
+  className: PropTypes.string,
+  style: PropTypes.object,
+}
+
+Navbar.defaultProps = {
+  logo: undefined,
+  children: undefined,
+  className: undefined,
+  style: undefined,
+}
+
+export default Navbar
