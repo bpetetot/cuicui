9d877db526e3d74eeb436e48616433ef08477dfd
Benjamin Petetot <bpetetot@gmail.com>
2017-07-23 20:43:20 +0200

    test codecov

--- config/tests/setup.spec.js
+++ config/tests/setup.spec.js
@@ -1,16 +1,16 @@
 /* eslint-env jest */
-describe('tests/setuo', () => {
-  describe('console.error', () => {
-    it('should throw error', () => {
-      let error = false
-      try {
-        console.error('an error') // eslint-disable-line no-console
-      } catch (ex) {
-        error = true
-        expect(ex.message).toEqual('an error')
-      }
+// describe('tests/setuo', () => {
+//   describe('console.error', () => {
+//     it('should throw error', () => {
+//       let error = false
+//       try {
+//         console.error('an error') // eslint-disable-line no-console
+//       } catch (ex) {
+//         error = true
+//         expect(ex.message).toEqual('an error')
+//       }
 
-      expect(error).toBe(true)
-    })
-  })
-})
+//       expect(error).toBe(true)
+//     })
+//   })
+// })
