a0f860d8158b285b4c5d1d4913ae23b64c1890e5
Benjamin Petetot <bpetetot@gmail.com>
2017-07-27 16:30:40 +0200

    :bug: fix elements

--- docs/src/samples/tabs/basic/basic.jsx
+++ docs/src/samples/tabs/basic/basic.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import source from 'raw-loader!./basic.sample.jsx'
-import TabSample from 'babel-loader!./basic.sample.jsx'
+import BasicSample from 'babel-loader!./basic.sample.jsx'
 
 import Sample from '../../../components/sample'
 
@@ -11,7 +11,7 @@ const Basic = () => {
       <h2>Simple Tabs</h2>
       <p>A simple Tabs component displaying tab content on click.</p>
       <Sample code={source} githubLink="https://github.com/bpetetot/cuicui/blob/master/docs/src/samples/tabs/basic/basic.sample.jsx">
-        <TabSample />
+        <BasicSample />
       </Sample>
     </div>
   )
